ad75c5ccb289d3a1c7073ff13817baf0eef1a74d
azhukova <zhutchok@gmail.com>
2025-05-06 15:40:29 +0200

    Fixed logging parameters for 1-state models (no frequency logging)

--- treesimulator/__init__.py
+++ treesimulator/__init__.py
@@ -38,19 +38,22 @@ def save_log(models, skyline_times, total_tips, T, u, log, kappa=0, observed_fre
     skyline_times += [T]
     is_ct = isinstance(models[0], CTModel)
     with open(log, 'w+') as f:
-        pis = [f'pi_{s}' for s in models[0].states]
+        states = models[0].states
+        pis = [f'pi_{s}' for s in states]
         keys = sorted([k for k in models[0].get_epidemiological_parameters().keys() if k not in set(pis)],
                       key=lambda _: (_.split('_')[0], _))
 
-        f.write('{}{},{},tips,hidden_trees,end_time\n'\
+        f.write('{}{},{}tips,hidden_trees,end_time\n'\
                 .format(','.join(keys),
                         ',kappa' if is_ct else '',
-                        ','.join(f'{pi},{pi}_observed' for pi in pis)))
+                        (','.join(f'{pi},{pi}_observed' for pi in pis) + ',') if len(states) > 1 else ''))
         for model, end_time, obs in zip(models, skyline_times, observed_frequencies):
             tips = '' if end_time < T else total_tips
             params = model.get_epidemiological_parameters()
-            f.write('{}{},{},{},{},{:g}\n'.format(','.join(f'{params[k]:g}' for k in keys),
-                                               f',{kappa:g}' if is_ct else '',
-                                               ','.join(f'{pi:g},{o:g}' for (pi, o) in zip((params[_] for _ in pis), obs)),
-                                               tips, u, end_time))
+            f.write('{}{},{}{},{},{:g}\n'
+                    .format(','.join(f'{params[k]:g}' for k in keys),
+                            f',{kappa:g}' if is_ct else '',
+                            (','.join(f'{pi:g},{o:g}' for (pi, o) in zip((params[_] for _ in pis), obs)) + ',')
+                                                if len(states) > 1 else '',
+                            tips, u, end_time))
             u = ''
